8c7cd686892b8c23189b154067008032931e5e63
iofjuupasli <iofjuupasli@gmail.com>
2017-11-30 16:29:55 +0300

    update versions; eslint v4; add indent rules

--- client.js
+++ client.js
@@ -42,7 +42,26 @@ module.exports = {
         "indent": [
             "error",
             4, {
-                "SwitchCase": 1
+                SwitchCase: 1,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1,
+                // MemberExpression: null,
+                FunctionDeclaration: {
+                    parameters: 1,
+                    body: 1
+                },
+                FunctionExpression: {
+                    parameters: 1,
+                    body: 1
+                },
+                CallExpression: {
+                    arguments: 1
+                },
+                ArrayExpression: 1,
+                ObjectExpression: 1,
+                ImportDeclaration: 1,
+                flatTernaryExpressions: false,
+                ignoredNodes: ['JSXElement', 'JSXElement *']
             }
         ],
         "react/jsx-indent": [
@@ -62,7 +81,10 @@ module.exports = {
                 ]
             }
         ],
-        "filenames/match-exported": "error",
+        "filenames/match-exported": [
+            "error",
+            ["kebab", "camel", "pascal", null]
+        ],
         "import/no-extraneous-dependencies": 0,
         "jsx-a11y/label-has-for": 0,
         "jsx-a11y/no-static-element-interactions": 0,
