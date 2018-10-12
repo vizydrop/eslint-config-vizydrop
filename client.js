9658f18fdd960c9118abcf92e555146d1a3c1f8e
iofjuupasli <iofjuupasli@gmail.com>
2018-10-12 17:13:17 +0300

    5.0.0 - server same as client

--- client.js
+++ client.js
@@ -1,71 +1,14 @@
+const serverConfig = require(`./server`);
+
 module.exports = {
-    "extends": "airbnb",
-    "parser": "babel-eslint",
-    "plugins": [
-        "flowtype",
-        "filenames",
-        "class-property",
-        "lodash-fp"
-    ],
-    "settings": {
-        "import/resolver": {
-            "node": {
-                "moduleDirectory": ["node_modules"]
-            }
-        }
-    },
-    "env": {
-        "node": true,
-        "browser": true,
-        "mocha": true,
-        "es6": true,
-        "jest": true
-    },
+    ...serverConfig,
     "globals": {
-        "__taus": true
+        ...serverConfig.globals,
+        "__taus": true,
+        "__track": true
     },
     "rules": {
-        'max-len': [
-            "error",
-            120,
-            4, {
-                "ignoreUrls": true,
-                "ignoreComments": false,
-                "ignoreRegExpLiterals": true,
-                "ignoreStrings": true,
-                "ignoreTemplateLiterals": true
-            }
-        ],
-        "quotes": [
-            "error", "backtick"
-        ],
-        "prefer-destructuring": 0,
-        "function-paren-newline": ["error", "consistent"],
-        "indent": [
-            "error",
-            4, {
-                SwitchCase: 1,
-                VariableDeclarator: 1,
-                outerIIFEBody: 1,
-                // MemberExpression: null,
-                FunctionDeclaration: {
-                    parameters: 1,
-                    body: 1
-                },
-                FunctionExpression: {
-                    parameters: 1,
-                    body: 1
-                },
-                CallExpression: {
-                    arguments: 1
-                },
-                ArrayExpression: 1,
-                ObjectExpression: 1,
-                ImportDeclaration: 1,
-                flatTernaryExpressions: false,
-                ignoredNodes: ['JSXElement', 'JSXElement *']
-            }
-        ],
+        ...serverConfig.rules,
         "react/jsx-indent": [
             "error", 4
         ],
@@ -75,6 +18,9 @@ module.exports = {
         "react/sort-comp": [
             "error", {
                 order: [
+                    "displayName",
+                    "defaultProps",
+                    "propTypes",
                     "type-annotations",
                     "static-methods",
                     "lifecycle",
@@ -83,11 +29,6 @@ module.exports = {
                 ]
             }
         ],
-        "filenames/match-exported": [
-            "error",
-            ["kebab", "camel", "pascal", null]
-        ],
-        "import/no-extraneous-dependencies": 0,
         "jsx-a11y/label-has-for": 0,
         "jsx-a11y/no-static-element-interactions": 0,
         "jsx-a11y/no-noninteractive-element-interactions": 0,
@@ -95,64 +36,16 @@ module.exports = {
         "jsx-a11y/click-events-have-key-events": 0,
         "jsx-a11y/jsx-a11y/mouse-events-have-key-events": 0,
         "jsx-a11y/mouse-events-have-key-events": 0,
-        "import/prefer-default-export": 0,
         "react/forbid-prop-types": 0,
         "react/no-danger": 0,
         "react/default-props-match-prop-types": 1,
-        "class-methods-use-this": 0,
         "react/require-default-props": 0,
         "react/no-array-index-key": 0,
         "react/prefer-stateless-function": 0,
         "no-underscore-dangle": [
             "error", {
-                "allow": ["__taus"]
+                "allow": ["__taus", "__track"]
             }
-        ],
-        "object-curly-spacing": [
-            "error", "never"
-        ],
-        "arrow-parens": [
-            "error", "always"
-        ],
-        "class-property/class-property-semicolon": [
-            "error", "always"
-        ],
-        "lodash-fp/consistent-compose": [
-            "error", "pipe"
-        ],
-        "lodash-fp/consistent-name": [
-            "error", "_"
-        ],
-        "lodash-fp/no-argumentless-calls": "error",
-        "lodash-fp/no-chain": "error",
-        "lodash-fp/no-extraneous-args": "error",
-        "lodash-fp/no-extraneous-iteratee-args": "error",
-        "lodash-fp/no-partial-of-curried": "error",
-        "lodash-fp/no-single-composition": "error",
-        "lodash-fp/prefer-compact": "error",
-        "lodash-fp/prefer-flat-map": "error",
-        "lodash-fp/prefer-get": "error",
-        "lodash-fp/use-fp": "error",
-        "flowtype/boolean-style": "error",
-        "flowtype/define-flow-type": "error",
-        "flowtype/delimiter-dangle": [
-            "error", "only-multiline"
-        ],
-        "flowtype/generic-spacing": [
-            "error", "never"
-        ],
-        "flowtype/no-dupe-keys": "error",
-        "flowtype/no-primitive-constructor-types": "error",
-        "flowtype/object-type-delimiter": [
-            "error", "comma"
-        ],
-        "flowtype/semi": "error",
-        "flowtype/space-after-type-colon": [
-            "error", "always"
-        ],
-        "flowtype/space-before-generic-bracket": "error",
-        "flowtype/space-before-type-colon": "error",
-        "flowtype/union-intersection-spacing": "error",
-        "flowtype/use-flow-type": "error"
+        ]
     }
 };
--- server.js
+++ server.js
@@ -1,60 +1,138 @@
 module.exports = {
-    "extends": "standard",
+    "extends": "airbnb",
     "parser": "babel-eslint",
     "plugins": [
         "flowtype",
-        "standard",
-        "mocha"
+        "filenames",
+        "class-property",
+        "lodash-fp"
     ],
-    "parserOptions": {
-        "ecmaVersion": 2017,
-    },
     "env": {
         "node": true,
-        "mocha": true,
+        "browser": true
     },
+    "overrides": [
+        {
+            "files": [
+                "test.js",
+                "*.test.js",
+                "*.test.jsx",
+                "**/__tests__/**.js",
+                "**/__mocks__/**.js",
+                "**/__tests__/**.jsx",
+                "**/tests/**/*.js"
+            ],
+            "env": {
+                "node": true,
+                "browser": true,
+                "mocha": true,
+                "jest": true
+            },
+        }
+    ],
     "rules": {
-        "semi": [
-            "error", "always"
+        "max-depth": [1, 4],
+        "max-nested-callbacks": [1, 4],
+        "max-params": [1, 4],
+        "max-statements": [1, 10],
+        'max-len': [
+            "error",
+            120,
+            4, {
+                "ignoreUrls": true,
+                "ignoreComments": false,
+                "ignoreRegExpLiterals": true,
+                "ignoreStrings": true,
+                "ignoreTemplateLiterals": true
+            }
         ],
+        "no-plusplus": 0,
+        "quotes": [
+            "error", "backtick"
+        ],
+        "prefer-destructuring": 0,
+        "function-paren-newline": ["error", "consistent"],
         "indent": [
             "error",
             4, {
-                "SwitchCase": 1
+                SwitchCase: 1,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1,
+                // MemberExpression: null,
+                FunctionDeclaration: {
+                    parameters: 1,
+                    body: 1
+                },
+                FunctionExpression: {
+                    parameters: 1,
+                    body: 1
+                },
+                CallExpression: {
+                    arguments: 1
+                },
+                ArrayExpression: 1,
+                ObjectExpression: 1,
+                ImportDeclaration: 1,
+                flatTernaryExpressions: false,
+                ignoredNodes: ['JSXElement', 'JSXElement *']
             }
         ],
-        "spaced-comment": [
-            "off", "never"
+        "filenames/match-exported": [
+            "error",
+            ["kebab", "camel", "pascal", null]
         ],
-        "space-before-function-paren": [
-            "error", {
-                "named": "never",
-                "anonymous": "always"
-            }
+        "import/no-extraneous-dependencies": 0,
+        "import/prefer-default-export": 0,
+        "class-methods-use-this": 0,
+        "no-underscore-dangle": 2,
+        "object-curly-spacing": [
+            "error", "never"
         ],
-        "mocha/no-exclusive-tests": ["error"],
         "arrow-parens": [
             "error", "always"
         ],
-        "complexity": [
-            "warn",
-            5
+        "class-property/class-property-semicolon": [
+            "error", "always"
         ],
-        "max-depth": [
-            "warn",
-            4
+        "lodash-fp/consistent-compose": [
+            "error", "pipe"
         ],
-        "max-nested-callbacks": [
-            "warn",
-            4
+        "lodash-fp/consistent-name": [
+            "error", "_"
         ],
-        "max-params": [
-            "warn",
-            5
+        "lodash-fp/no-argumentless-calls": "error",
+        "lodash-fp/no-chain": "error",
+        "lodash-fp/no-extraneous-args": "error",
+        "lodash-fp/no-extraneous-iteratee-args": "error",
+        "lodash-fp/no-partial-of-curried": "error",
+        "lodash-fp/no-single-composition": "error",
+        "lodash-fp/prefer-compact": "error",
+        "lodash-fp/prefer-flat-map": "error",
+        "lodash-fp/prefer-get": "error",
+        "lodash-fp/use-fp": "error",
+        "lodash-fp/preferred-alias": ["error", {overrides: ["pipe", "first"]}],
+        "flowtype/boolean-style": "error",
+        "flowtype/define-flow-type": "error",
+        "flowtype/delimiter-dangle": [
+            "error", "only-multiline"
         ],
-        "max-statements": [
-            "warn",
-            20
-        ]
+        "flowtype/generic-spacing": [
+            "error", "never"
+        ],
+        "flowtype/no-dupe-keys": "error",
+        "flowtype/no-primitive-constructor-types": "error",
+        "flowtype/object-type-delimiter": [
+            "error", "comma"
+        ],
+        "flowtype/semi": "error",
+        "flowtype/space-after-type-colon": [
+            "error", "always"
+        ],
+        "flowtype/space-before-generic-bracket": "error",
+        "flowtype/space-before-type-colon": "error",
+        "flowtype/union-intersection-spacing": "error",
+        "flowtype/use-flow-type": "error",
+        "flowtype/array-style-complex-type": "error",
+        "flowtype/no-types-missing-file-annotation": "error"
     }
 };
